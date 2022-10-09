b7e2cb7e29625bc885513de8674c7e7230719085
amitai <dan@brandtlautman.com>
2022-10-09 18:40:02 -0400

    simplify, man

--- index.php
+++ index.php
@@ -1,7 +1,5 @@
 <html>
 <head>
-<link rel="stylesheet" href="efe.css">
-<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 <script>
 <?php 
     $videolar_ad=array();
@@ -63,44 +61,13 @@ function ooops() {
     console.log(document.getElementById("play-video").error.code)
 }
 </script>
-    <style>
-    video#backgroundvid {
-position: fixed; right: 0; bottom: 0;
-min-width: 100%; min-height: 100%;
-width: auto; height: auto; z-index: -100;
-background: url(polina.jpg) no-repeat;
-background-size: cover;
-}
-    </style>
     </head>
 
     
     <body> 
-    <div class="existing_items">
-        <table cellspacing="0">
-            <th class="pointer"colspan="2">
-                <nav class="navbar navbar-expand-lg navbar-light bg-light">
-                <?php 
-  if(isset($_POST["submit"])){
-  $link = $_POST["link"];
-  echo '<a href="'.$link.'">Siteye Git</a>';// corrected incorrect usage of quotes
-  }
-  ?>
-  <form accept="" action="" method="post"> <!-- added form open -->       
-  <input type="url" class="form-control" name="link" placeholder="Enter Product URL" required>
-  <input class="button" type="submit" name="submit" value="Submit">
-  </form>
-                </nav>
-            </th>
-            <tr><th class="" colspan="2"></th></tr>
-        </table>
-        </div>
 <div class="video-player">
-    <video  class="videom" id="play-video" width="%100" height="%100" controls autobuffer muted id="backgroundvid"></video>
+    <video  class="videom" id="play-video" controls autobuffer id="backgroundvid"></video>
 </div>  
-<div
-    class="container">
-</div>
 <script>
     document.getElementById("play-video").addEventListener('error', ooops,false);
 
